9310ed9182c878e246b2626fea2384bf72b1e26f
Davi Domingos de Oliveira <odomingosdavi@gmail.com>
2025-09-25 20:15:57 -0300

    Create SwitchMes.go

---
+++ Aula 2/SwitchMes.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+const Pi = 3.14159
+
+func main() {
+	var mes int
+	println("Digite um número correspondente ao mes:")
+	fmt.Scanf("%d", &mes)
+
+	switch mes {
+	case 1:
+		fmt.Println("Janeiro")
+	case 2:
+		fmt.Println("Fevereiro")
+	case 3:
+		fmt.Println("Março")
+	case 4:
+		fmt.Println("Abril")
+	case 5:
+		fmt.Println("Maio")
+	case 6:
+		fmt.Println("Junho")
+	case 7:
+		fmt.Println("Julho")
+	case 8:
+		fmt.Println("Agosto")
+	case 9:
+		fmt.Println("Setembro")
+	case 10:
+		fmt.Println("Outubro")
+	case 11:
+		fmt.Println("Novembro")
+	case 12:
+		fmt.Println("Dezembro")
+	default:
+		fmt.Println("Mês inválido")
+	}
+
+}
