15592d1e5290964d87cf636f0f8d98759b12bcb8
Davi Domingos de Oliveira <odomingosdavi@gmail.com>
2025-09-25 19:56:49 -0300

    Create NomeIdadeAltura.go

---
+++ Aula 2/NomeIdadeAltura.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+const Pi = 3.14159
+
+func main() {
+	var nome string
+	var idade int
+	var altura uint16
+	fmt.Println("Insira seu nome, altura(Centimetros) e idade")
+	fmt.Scanf("%s\n", &nome)
+	fmt.Scanf("%d\n", &altura)
+	fmt.Scanf("%d\n", &idade)
+
+	fmt.Printf("Seu nome é %s, você tem %d anos e sua altura é %d centimetros\n", nome, idade, altura)
+	if idade < 18 {
+		fmt.Println("Voce e de menor")
+	} else {
+		fmt.Println("Voce e de maior")
+	}
+}
