633959c7ebddfc32903a89e2c01338d90116a1c5
Davi Domingos de Oliveira <odomingosdavi@gmail.com>
2025-09-30 19:49:31 -0300

    Update funcaocomretornocomposto.go

--- Aula 3 /funcaocomretornocomposto.go
+++ Aula 3 /funcaocomretornocomposto.go
@@ -3,13 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	valido, resultado := divisao(10, 0)
-	fmt.Printf("O calculo é válido? %v, o resultado é: %.2f", valido, resultado)
+	valido, resultado, operacao := divisao(10, 0)
+	fmt.Printf("A operação é: %s, O calculo é válido? %v, o resultado é: %.2f", operacao, valido, resultado)
 }
 
-func divisao(a float64, b float64) (bool, float64) {
+func divisao(a float64, b float64) (bool, float64, string) {
 	if b == 0 {
-		return false, 0
+		return false, 0, "divisão"
 	}
-	return true, a / b
+	return true, a / b, "divisão"
 }
