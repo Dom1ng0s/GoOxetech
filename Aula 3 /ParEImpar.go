44eca38475462fa8cf8052c881ffaa2896ea09df
Davi Domingos de Oliveira <odomingosdavi@gmail.com>
2025-09-30 19:57:33 -0300

    Create ParEImpar.go

---
+++ Aula 3 /ParEImpar.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var numero int64
+	fmt.Scanf("%d", &numero)
+	parEImpar(int(numero))
+}
+
+func parEImpar(a int) {
+	for i := 0; i < a; i++ {
+		if i%2 == 0 {
+			fmt.Printf("%d é par\n", i)
+		} else {
+			fmt.Printf("%d é impar\n", i)
+		}
+
+	}
+}
