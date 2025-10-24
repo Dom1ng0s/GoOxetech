fcaf60486bc26733a9d9412194dbc33a4c9a6ad8
Davi Domingos de Oliveira <odomingosdavi@gmail.com>
2025-10-24 08:55:05 -0300

    Update area.go

--- Aula 5/area.go
+++ Aula 5/area.go
@@ -7,26 +7,32 @@ type Retangulo struct {
 	Largura     int
 }
 
-func Area(Operando Retangulo) int {
-	areaOperando := Operando.Comprimento * Operando.Largura
-	fmt.Printf("A Area do Retângulo com dimensões: %d e %d é %d\n", Operando.Comprimento, Operando.Largura, areaOperando)
+func (r Retangulo) Area() int {
+	areaOperando := r.Comprimento * r.Largura
+	fmt.Printf("A Área do Retângulo com dimensões: %d e %d é %d\n", r.Comprimento, r.Largura, areaOperando)
 	return areaOperando
 }
-func DobrarArea(Operando Retangulo) int {
-	NovoOperando := Retangulo{Comprimento: Operando.Comprimento * 2, Largura: Operando.Largura * 2}
-	fmt.Print("Novo Retângulo:")
-	return Area(NovoOperando)
 
+func (r Retangulo) DobrarArea() int {
+	NovoOperando := Retangulo{Comprimento: r.Comprimento * 2, Largura: r.Largura * 2}
+	fmt.Print("Novo Retângulo:")
+	return NovoOperando.Area()
 }
 
-func main() {
+func AreaRetangulo() {
 	ret1 := Retangulo{Comprimento: 10, Largura: 20}
 	ret2 := Retangulo{Comprimento: 20, Largura: 30}
 	ret3 := Retangulo{Comprimento: 30, Largura: 40}
-	Area(ret1)
-	Area(ret2)
-	Area(ret3)
-	DobrarArea(ret1)
-	DobrarArea(ret2)
-	DobrarArea(ret3)
+
+	ret1.Area()
+	ret2.Area()
+	ret3.Area()
+
+	ret1.DobrarArea()
+	ret2.DobrarArea()
+	ret3.DobrarArea()
+}
+
+func main() {
+    AreaRetangulo()
 }
